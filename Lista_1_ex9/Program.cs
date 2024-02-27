bd16e55e86c77bbe82dc8b935da1537191432abd
mentarios <matmentarios@gmail.com>
2024-02-27 14:52:50 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("digite um numero:");
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0) { Console.WriteLine("seu numero é par"); }
+            else { Console.WriteLine("seu numero é impar"); }
+        }
+    }
+}
